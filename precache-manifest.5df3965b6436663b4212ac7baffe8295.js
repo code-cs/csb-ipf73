dd54f0af6c220adbb75151a1d859bff7d5365ec8
Code Sandbox <hello@codesandbox.io>
2021-05-09 10:31:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5df3965b6436663b4212ac7baffe8295.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42f878da9079729f58188829388c2fa",
+    "url": "https://code-cs.github.io/csb-ipf73/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "https://code-cs.github.io/csb-ipf73/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "653f73d14d3745240ff8",
+    "url": "https://code-cs.github.io/csb-ipf73/static/js/main.a2469041.chunk.js"
+  },
+  {
+    "revision": "5194b451d757b3d55337",
+    "url": "https://code-cs.github.io/csb-ipf73/static/js/runtime-main.fc5dc903.js"
+  }
+]);
